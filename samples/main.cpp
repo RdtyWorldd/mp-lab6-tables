293d951a0bda3c9f39f4df4d44950a05ac309501
Никита Морозов <rdtyworldd@MacBook-Air-Nikita.local>
2025-04-26 17:52:53 +0300

    Realize sort methods in SortTable

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <ScanTable.h>
+#include <SortTable.h>
 #include <random>
 
 int main()
 {
-    int size = 5;
-    Table<int, int>* table = new ScanTable<int, int>();
-
+    int size = 6;
+    Table<int, int>* table = new ScanTable<int, int>(size);
+    Table<int, int>* table1;
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -16,13 +17,22 @@ int main()
 
     for(int i = 0; i < size; i++)
     {
-        table->insert(idit(gen), i);
+        int key = idit(gen);
+        std::cout << key << ' ';
+        table->insert(key, i);
     }
-
+    std::cout << std::endl;
 
     for(table->reset(); !table->is_end(); table->go_next())
     {
         std::cout << table->get_curr_key() << " " << table->get_curr_val() << std::endl;
+    }
+    std::cout << std::endl;
+
+    table1 = new SortTable<int, int>(*((ScanTable<int, int> *)table));
+    for(table1->reset(); !table1->is_end(); table1->go_next())
+    {
+        std::cout << table1->get_curr_key() << " " << table1->get_curr_val() << std::endl;
     }
 
     return 0;
--- src/SortTable.h
+++ src/SortTable.h
@@ -8,12 +8,13 @@ class SortTable : public ScanTable<Key, Value>
 private:
     typedef typename Table<Key, Value>::Record Record;
 //i forget for what we write this methods (may be to cast unsotred table to sort)
-    // void select_sort();
-    // void quick_sort(int start, int finish);
-    // void merge_sort();
-    // void merge();
+    void sort(int nlogn = 0);
+    void select_sort(); 
+    void quick_sort(int start, int finish); 
+    void merge_sort(int start, int finish); 
+    void merge(int, int, int, int); 
 public:
-    SortTable(int _size);
+    SortTable(int _size = 10);
     SortTable(const SortTable& table);
     SortTable(const ScanTable<Key, Value>& table);
 
@@ -24,60 +25,160 @@ public:
     virtual void del(Key key);
 };
 
-// template <typename Key, typename Value>
-// inline void SortTable<Key, Value>::select_sort()
-// {
-//     for(int i = 0; i < arr_size-1; i++)
-//     {
-//         Record m = pRec[i];
-//         for(int j = i+1; i < arr_size; j++)
-//         {
-//             if(m > pRec[j])
-//             {
-//                 m = pRec[j];
-//                 pRec[j] = pRec[i];   
-//             }
-//         }
-//         arr[i] = m;
-//     }
-// }
+template <typename Key, typename Value>
+inline void SortTable<Key, Value>::sort(int k)
+{
+    std::cout<< "in sort\n";
+    switch (k)
+    {
+    case 0:
+        select_sort();
+        break;
+    case 1:
+        quick_sort(0, this->el_count-1);
+        break;
+    case 2:
+        merge_sort(0, this->el_count-1);
+        break;
+    default:
+        throw std::invalid_argument("not a number of sort");
+        break;
+    }
+}
 
-// template <typename Key, typename Value>
-// inline void SortTable<Key, Value>::quick_sort(int start, int finish)
-// {
-//     int k = (start + finish) / 2;
-//     int left = start; 
-//     int right = finish;
-//     Record partion = pRec[k];
-//     while(left < right)
-//     {
-//         while(pRec[left]  < partion)
-//             left ++;
-//         while(pRec[right] > partion)
-//             right--;
-//         if(left <= right)
-//         {
-//             //swap
-//             left++; 
-//             right--;
-//         }
-//     }
-//     if(start < left)
-//     {
-//         quick_sort(start, left);
-//         quick_sort(right, finish);
-//     } 
-// }
+template <typename Key, typename Value>
+inline void SortTable<Key, Value>::select_sort()
+{ 
+    for(int i = 0; i < this->el_count-1; i++)
+    {
+        Record m = this->pRec[i];
+        for(int j = i+1; j < this->el_count; j++)
+        {
+            if(m.key > this->pRec[j].key)
+            {
+                m = this->pRec[j];
+                this->pRec[j] = this->pRec[i];   
+            }
+        }
+        this->pRec[i] = m;
+    }
+}
+
+template <typename Key, typename Value>
+inline void SortTable<Key, Value>::quick_sort(int start, int finish)
+{
+    int k = (start + finish) / 2;
+    int left = start; 
+    int right = finish;
+    Record partion = this->pRec[k];
+    while(left < right)
+    {
+        while(this->pRec[left].key  < partion.key)
+            left ++;
+        while(this->pRec[right].key > partion.key)
+            right--;
+        if(left <= right)
+        {
+            Record tmp = this->pRec[left];
+            this->pRec[left] = this->pRec[right];
+            this->pRec[right] = tmp;
+            left++; 
+            right--;
+        }
+    }
+    if(start < left)
+    {
+        if((start+finish) & 1) 
+        {
+            quick_sort(start, right);
+            quick_sort(left, finish);
+        }
+        else
+        {
+            quick_sort(start, right+1);
+            quick_sort(left, finish);
+        }
+        
+    } 
+}
+
+template <typename Key, typename Value>
+inline void SortTable<Key, Value>::merge_sort(int start, int finish)
+{
+    int size = finish - start;
+    int k = size / 2;
+    std::cout<< start << finish << size << k << std::endl;
+    if(size < 1)
+    {
+        return;
+    }
+
+    merge_sort(start, start+k);
+    merge_sort(start+k+1, finish);
+    merge(start, start+ k, start + k + 1, finish);
+}
+
+template <typename Key, typename Value>
+inline void SortTable<Key, Value>::merge(int s_a, int f_a, int s_b, int f_b)
+{
+    int i = s_a, j = s_b, k = 0;
+    int size = (f_a - s_a) + (f_b - s_b) + 2;
+    std::cout << size << std::endl;
+    Record* tmp = new Record[size];
+    while(i <= f_a && j <= f_b)
+    {
+        std::cout<< "merge :" << s_a << "i:" << i << "j:" << j << std::endl;
+        if(this->pRec[i].key <= this->pRec[j].key)
+        {
+            tmp[k] = this->pRec[i];
+            i++;
+        }
+        else
+        {
+            tmp[k] = this->pRec[j];
+            j++;
+        }
+        k++;
+    }
+
+    for( ; i <= f_a; i++)
+    {
+        tmp[k] = this->pRec[i];
+        k++;
+    }
+
+    for( ; j <= f_b; j++)
+    {
+        tmp[k] = this->pRec[j];
+        k++;
+    }
+
+    for(k = 0; k < size; k++)
+    {
+        std::cout<< "merge :" << s_a << "iter:" << k << std::endl;
+        this->pRec[s_a + k] = tmp[k];
+    }
+    for(int k =0; k < size; k++)
+    {
+        std::cout << this->pRec[s_a + k].key << ' ';
+    }
+    std::cout<< std::endl;
+}
+
+//********************************
 
 template <typename Key, typename Value>
 inline SortTable<Key, Value>::SortTable(int _size) : ScanTable<Key, Value>(_size) {}
 
 template <typename Key, typename Value>
-inline SortTable<Key, Value>::SortTable(const SortTable& table) : ScanTable<Key, Value>(table) {}
+inline SortTable<Key, Value>::SortTable(const SortTable& table) : ScanTable<Key, Value>(table) {
+    std::cout<< "sort table\n";
+}
 
 template<typename Key, typename Value>
 inline SortTable<Key, Value>::SortTable(const ScanTable<Key, Value>& table): ScanTable<Key, Value>(table) {
-    //some sort method
+    sort(1);
+    std::cout<< "sort table\n";
 }
 
 template <typename Key, typename Value>
